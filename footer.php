2c2077a91c48c6ab222ae7bd3527629feb46e4dd
RowanLodewijks <210548@student.glu.nl>
2021-11-11 14:38:07 +0100

    2e commit

---
+++ footer.php
@@ -0,0 +1,23 @@
+<div id="footer">
+    	<div class="area">
+        <?php dynamic_sidebar( 'zijkant_widget' ); ?>
+
+        <h2>Informatie</h2>
+        <img src="<?php bloginfo( 'template_url' ); ?>/imgs/tents.jpg" class="show_picture" />
+        <h3>Landerij Wildvecht</h3>
+
+        
+<p>
+Alle rechten voorbehouden.
+  Tarieven kunnen gewijzigd zijn.
+  Landerijen Wildvecht is een onderdeel van de Hollandse Groenlanden groep. 
+</p>
+
+
+        <div class="follow">
+          <b>Volg ons:</b>
+          <p>
+            <img src="<?php bloginfo( 'template_url' ); ?>/imgs/socials.jpg" />
+          </p>
+        </div>
+    </div>
---
+++ functions.php
@@ -0,0 +1,18 @@
+<?php
+
+function registeer_widgets() {
+    register_sidebar(
+        array(
+        'id' => 'zijkant_widget',
+        'name' => "Mijn zijkant widget",
+        )
+    );
+}
+add_action( 'widgets_init', 'registeer_widgets' );
+ 
+function registeer_menus() {
+    register_nav_menu('hoofdmenu', 'Mijn Hoofdmenu');
+}
+add_action( 'init', 'registeer_menus' );
+
+?>
